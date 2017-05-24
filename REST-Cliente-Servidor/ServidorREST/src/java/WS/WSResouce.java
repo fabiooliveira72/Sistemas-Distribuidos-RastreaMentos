ebb2eac7d78f828f8b42a28e5eaa7324d973f1e3
JisseleadeCarli <jisseleadecarli@outlook.com>
2017-05-24 08:17:41 -0300

    V2 GET listaTipo

--- REST-Cliente-Servidor/ServidorREST/src/java/WS/WSResouce.java
+++ REST-Cliente-Servidor/ServidorREST/src/java/WS/WSResouce.java
@@ -1,13 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package WS;
 
-
+import Banco.Conexao;
 import Banco.Operacoes;
-import Entidades.Posicao;
 import Entidades.Veiculo;
 import com.google.gson.Gson;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -17,7 +19,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
-import java.util.Date;
 
 /**
  * REST Web Service
@@ -52,32 +53,9 @@ public class WSResouce {
         Gson g = new Gson();
         return g.toJson(v);
 //TODO return proper representation object
-        //throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();   
     }
     
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("WSRRastreamento/getListaTipo/{tipo}")
-    public String listTipoVeiculo(@PathParam("tipo") Integer tipo) throws SQLException {
-         List<Veiculo> lista = new ArrayList();
-        lista = Operacoes.listaTipo(tipo);
-        Gson g = new Gson();
-        return g.toJson(lista);
-//TODO return proper representation object
-        //throw new UnsupportedOperationException();
-    }  
-    
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Path("WSRRastreamento/getLocalizacao/{codigo}/{datahora}")
-    public String listTipoVeiculo(@PathParam("tipo") Integer tipo,Date datahora) throws SQLException {
-         List<Posicao> lista = new ArrayList();
-        lista = Operacoes.listaPosicaoVeiculo(tipo, datahora);
-        Gson g = new Gson();
-        return g.toJson(lista);
-//TODO return proper representation object
-        //throw new UnsupportedOperationException();
-    }
     /**
      * PUT method for updating or creating an instance of WSResouce
      * @param content representation for the resource

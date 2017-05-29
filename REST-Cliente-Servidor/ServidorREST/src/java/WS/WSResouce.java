6153b21f387ac8ef0f98bb1797e17f738b7a9f96
JisseleadeCarli <jisseleadecarli@outlook.com>
2017-05-29 00:13:34 -0300

    POST

--- REST-Cliente-Servidor/ServidorREST/src/java/WS/WSResouce.java
+++ REST-Cliente-Servidor/ServidorREST/src/java/WS/WSResouce.java
@@ -10,19 +10,24 @@ import Banco.Operacoes;
 import Entidades.Posicao;
 import Entidades.Veiculo;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.jms.Message;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import static javax.ws.rs.HttpMethod.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import org.json.JSONObject;
 
 /**
  * REST Web Service
@@ -48,7 +53,7 @@ public class WSResouce {
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("WSRRastreamento/getVeiculo/{codigo}")
+    @Path("getVeiculo/{codigo}")
     public String getVeiculo(@PathParam("codigo") Integer codigo) throws SQLException {
         Veiculo v = new Veiculo();
         //v.setCodigo(codigo);
@@ -59,9 +64,9 @@ public class WSResouce {
 //TODO return proper representation object
         //throw new UnsupportedOperationException();   
     }
-        @GET
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("WSRRastreamento/getListTypeVeiculo/{tipo}")
+    @Path("getListTypeVeiculo/{tipo}")
     public String listTipoVeiculo(@PathParam("tipo") Integer tipo) throws SQLException {
          List<Veiculo> lista = new ArrayList();
         lista = Operacoes.listaTipo(tipo);
@@ -69,16 +74,53 @@ public class WSResouce {
         return g.toJson(lista);
     } 
     
-        @GET
+    @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("WSRRastreamento/getListPosicao/{codigo}/{datahora}")
-     public String listTipoVeiculo(@PathParam("tipo") Integer tipo,Date datahora) throws SQLException {
+    @Path("getListPosicao/{codigo}/{datahora}")
+     public String listTipoVeiculo(@PathParam("tipo") Integer tipo,@PathParam("datahora") Date datahora) throws SQLException {
          List<Posicao> lista = new ArrayList();
         lista = Operacoes.listaPosicaoVeiculo(tipo, datahora);
         Gson g = new Gson();
         return g.toJson(lista);
     }
-    
+     
+    //@POST
+    //@Consumes(MediaType.APPLICATION_JSON)
+    //@Produces(MediaType.APPLICATION_JSON)
+    //@Path("postVeiculo")
+    //public String adicionaVeiculo(Gson paramJsonVeiculo) throws Exception{  
+     //   Gson JsonVeiculo = new Gson();
+    //     System.out.println(JsonVeiculo);
+    //    JsonVeiculo.toJson(paramJsonVeiculo);
+    //return JsonVeiculo.toString();
+    //public String adicionaVeiculo(@PathParam("tipo") Integer codigo, String placa, Integer tipo, Integer capacidade, String unpac ) throws SQLException {
+        //Veiculo v = new Veiculo();
+        //v = Operacoes.consultaVeiculo(codigo);
+     //se veiculo encontrado gera msg que não será incluido pq já existe, se não inclui veiculo
+        //v = new Veiculo(codigo, placa, tipo, capacidade, unpac); 
+        //Operacoes.adicionaVeiculo(v);
+        //Gson g = new Gson();
+        //return g.toJson("Veiculo Alterado");
+    //}
+    //}
+    @POST
+    @Path("/postVeiculo")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String postJson(String parVeiculo) throws Exception {
+        JSONObject parJsonVeiculo = new JSONObject(parVeiculo);
+        Veiculo v = new Veiculo();        
+        v.setCodigo((Integer) parJsonVeiculo.get("codigo"));
+        v.setPlaca(parJsonVeiculo.get("placa").toString());
+        v.setTipo((Integer) parJsonVeiculo.get("tipo"));
+        v.setCapacidade((Integer) parJsonVeiculo.get("capacidade"));
+        v.setUnpac(parJsonVeiculo.get("unpac").toString());
+        Operacoes.adicionaVeiculo(v);
+       Gson r = new Gson();
+        return r.toJson(v.getCapacidade());
+    }
+     
+     
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     public void putJson(String content) {
